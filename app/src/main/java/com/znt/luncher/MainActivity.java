0b18aae5c468e69817426fa83ecc757824c2f9bd
HotterYu <yuyan@zhunit.com>
2018-10-29 21:47:25 +0800

    增加了RePlugin

--- app/src/main/java/com/znt/luncher/MainActivity.java
+++ app/src/main/java/com/znt/luncher/MainActivity.java
@@ -4,13 +4,20 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.qihoo360.replugin.RePlugin;
+import com.qihoo360.replugin.model.PluginInfo;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,12 +28,15 @@ public class MainActivity extends Activity {
     private List<ResolveInfo> mlsResolveInfo;
     private RvTestAdapter mAdapter;
 
+    private Button btnInstall = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         mRvTest = findViewById(R.id.rv_test);
+        btnInstall = findViewById(R.id.btn_start_plugin);
         mlsResolveInfo = new ArrayList<>();
 
         mRvTest.setLayoutManager(new GridLayoutManager(this, 4));
@@ -39,6 +49,15 @@ public class MainActivity extends Activity {
             }
         });
 
+        btnInstall.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                openPluginApp();
+            }
+        });
+
         mRvTest.setAdapter(mAdapter);
 
         List<ResolveInfo> resolveInfos = queryMainActivitiesInfo();
@@ -47,6 +66,30 @@ public class MainActivity extends Activity {
 
     }
 
+    private void openPluginApp()
+    {
+
+        File file = new File(Environment.getExternalStorageDirectory() + "/app-debug.apk");
+        if(file.exists())
+        {
+            PluginInfo pluginInfo = RePlugin.install(file.getAbsolutePath());
+            RePlugin.preload(pluginInfo);//耗时
+        }
+
+        String pluginName = "com.znt.speaker";
+
+        // 若没有安装，则直接提示“错误”
+        // TODO 将来把回调串联上
+        if (RePlugin.isPluginInstalled(pluginName))
+        {
+            Log.e("","*********************startActivity");
+            RePlugin.startActivity(MainActivity.this, RePlugin.createIntent(pluginName, "com.znt.speaker.VideoPageActivity"));
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "You must install com.znt.speaker first!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     /**
      * 查询所有包含启动 intent 的 Activity 信息（去掉本应用）
---
+++ app/src/main/java/com/znt/luncher/MainApplication.java
@@ -0,0 +1,10 @@
+package com.znt.luncher;
+
+import com.qihoo360.replugin.RePluginApplication;
+
+/**
+ * Created by prize on 2018/10/29.
+ */
+
+public class MainApplication extends RePluginApplication {
+}
